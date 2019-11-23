92c090c9a8a80152995e905f919075f00194e673
ys01 <ys01@ys01.com>
2019-11-23 09:14:44 +0800

    添加SupplierDao.xml

--- src/main/java/com/zhg/javakc/modules/supply/supplier/controller/SupplierController.java
+++ src/main/java/com/zhg/javakc/modules/supply/supplier/controller/SupplierController.java
@@ -1,9 +1,16 @@
 package com.zhg.javakc.modules.supply.supplier.controller;
 
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.modules.supply.supplier.entity.SupplierEntity;
 import com.zhg.javakc.modules.supply.supplier.service.SupplierService;
+import com.zhg.javakc.modules.test.entity.TestEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author lenovo
@@ -16,4 +23,14 @@ public class SupplierController {
 
     @Autowired
     SupplierService supplierService;
+
+    @RequestMapping("/queryTest")
+    public ModelAndView queryTest(SupplierEntity supplierEntity, HttpServletRequest request, HttpServletResponse response){
+
+        ModelAndView modelAndView=new ModelAndView("test/list");
+        Page<SupplierEntity> page=supplierService.queryTest(supplierEntity,new Page<SupplierEntity>(request,response));
+        modelAndView.addObject("page",page);
+        return modelAndView;
+    }
+
 }
